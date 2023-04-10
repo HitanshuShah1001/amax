466445c651cd62463d77d9a258fc943b1c41c579
Hitanshu Shah <hitanshushah5@gmail.com>
2023-04-10 20:15:16 +0530

    Reusable component of image added

--- src/Aboutus/Aboutus.js
+++ src/Aboutus/Aboutus.js
@@ -11,47 +11,21 @@ export default function AboutUs() {
   return (
     <div
       className="about-us-container"
-      style={{ opacity: showContent ? 1 : 0, backgroundColor: "#F5F5F5" }}
+      style={{ opacity: showContent ? 1 : 0, backgroundColor: "white" }}
     >
       <div
         style={{
           display: "flex",
           justifyContent: "center",
-          margin: "20px 20px 20px",
         }}
       >
-        <img
-          src={require("../Carousel/Logo_withouttitle.png")}
-          style={{ height: 80, width: 120 }}
-          alt="Logo"
-        />
-      </div>
-      <img
-        className="about-us-image"
-        src={require("../Carousel/AboutUs.png")}
-        alt="About Us Logo"
-      />
-      <div className="about-us-content">
-        <p className="about-us-description">
-          We are proud to be one of the most renowned Manufacturers of quality
-          Road Equipment that is used by Contractors and Governments pan-India
-          and further exploring International markets. Our products are known
-          for their reliability, durability, and performance and we are
-          committed to providing our customers with the best products and
-          services possible. With about 21 years of experience in the industry,
-          our team of experts have developed a reputation for excellence and
-          performance of our products. We are constantly working to improve our
-          products and processes to meet the changing needs of our customers in
-          this competitive market.
-        </p>
-        <h4 className="about-us-difference-header">What makes us different?</h4>
-        <ul className="about-us-difference-list">
-          {Features.map((feature, id) => (
-            <div key={id}>
-              <li key={id}>{feature}</li>
-            </div>
-          ))}
-        </ul>
+        <div className="about-us-logo">
+          <img
+            src={require("../Carousel/Logo_withouttitle.png")}
+            style={{ height: 80, width: 120 }}
+            alt="Logo"
+          />
+        </div>
       </div>
     </div>
   );
